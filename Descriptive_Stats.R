448605261af72a676ff20ac5ab6bca1114673de6
Mamtabhishekverma <145756954+Mamtabhishekverma@users.noreply.github.com>
2024-05-27 17:50:57 +0530

    Add files via upload

---
+++ Descriptive_Stats.R
@@ -0,0 +1,78 @@
+#Read the csv file
+
+df <- datasets::airquality
+View(df)
+
+
+head(df) #First 6 rows
+head(df,2)
+tail(df,10)
+###########Summary of the data#########
+
+summary(df)
+df$Month
+df$Day
+df$Temp
+Temp
+summary(df$Temp)
+attach(df)
+Temp
+Month
+
+###########DATA VISUALIZATION IN R##########
+#Scatter plot
+
+plot(x = Wind,y = Temp)
+
+plot(Ozone, Solar.R, xlab = 'Ozone level',ylab = 'Solar',
+     main = 'Scatterplot Between Ozone vs Solar',
+     col = 'green',pch = 16)
+
+
+#Plot entire df
+
+plot(df)
+
+#Horizontal bar plot ##to know frequency of single value
+barplot(Month)
+unique(Month)
+table(Month)
+frequency <- table(Month)
+barplot(frequency)
+barplot(table(Month), col = 'green')
+
+
+#Histogram ##frequency of range of numbers
+hist(Ozone)
+summary(Ozone)
+hist(Temp,col = 'cyan')
+
+##Single box plot and stats
+
+boxplot(Ozone, col = '#e00720')
+boxplot(Ozone)$stats
+boxplot(Ozone)$out #to know value of outliers
+
+# Multiple box plots
+boxplot(df)
+
+#Multiple graphs in one canvas
+
+par(mfrow = c(2,2), mar = c(2,2,2,2))
+
+plot(Ozone,Temp,main = 'Scatterplot', pch = 19, col = 'red')
+hist(Ozone, col = 'green')
+boxplot(Ozone,col = 'blue')
+plot(Ozone, Solar.R, col = 'cyan')
+
+
+boxplot(Ozone, horizontal = T)
+
+#sd
+sd(Temp)
+mean(Temp)
+#var
+var(Temp)
+median(Temp)
+max(Temp)
+min(Temp)
